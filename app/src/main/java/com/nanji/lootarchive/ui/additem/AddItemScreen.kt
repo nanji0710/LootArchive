2a79f593ef7346b9c1c19d25e490ca3e01ce44ee
nanji0710 <2569232278@qq.com>
2026-07-21 13:55:19 +0800

    fix: v2.5.14 编译修复-重复参数

--- app/src/main/java/com/nanji/lootarchive/ui/additem/AddItemScreen.kt
+++ app/src/main/java/com/nanji/lootarchive/ui/additem/AddItemScreen.kt
@@ -250,7 +250,6 @@ fun AddItemScreen(
                                     },
                                     calendar.get(Calendar.YEAR),
                                     calendar.get(Calendar.MONTH),
-                                    calendar.get(Calendar.MONTH),
                                     calendar.get(Calendar.DAY_OF_MONTH)
                                 ).show()
                             }
