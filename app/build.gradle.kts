209396f60adbefce47b6b629bd9553331556050d
nanji0710 <2569232278@qq.com>
2026-07-21 10:28:37 +0800

    chore: v2.5.2 版本号更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nanji.lootarchive"
         minSdk = 31
         targetSdk = 36
-        versionCode = 27
-        versionName = "2.5.1"
+        versionCode = 28
+        versionName = "2.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
