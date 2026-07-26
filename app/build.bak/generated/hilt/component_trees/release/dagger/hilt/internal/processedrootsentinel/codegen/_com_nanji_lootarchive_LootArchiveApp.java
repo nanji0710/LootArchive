58e023142d177b771760268d1520e563c385c4e4
nanji0710 <2569232278@qq.com>
2026-07-26 21:40:08 +0800

    chore: remove accidentally committed build.bak

--- app/build.bak/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_nanji_lootarchive_LootArchiveApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.nanji.lootarchive.LootArchiveApp"
-)
-public final class _com_nanji_lootarchive_LootArchiveApp {
-}
