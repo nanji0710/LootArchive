22d22567c147ddf708401692cdd6fcabf3969153
ckl <nanji@daikyn.com>
2026-07-28 19:01:07 +0800

    v5.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.nanji.lootarchive"
         minSdk = 31
         targetSdk = 36
-        versionCode = 501
-        versionName = "5.0.1"
+        versionCode = 502
+        versionName = "5.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
