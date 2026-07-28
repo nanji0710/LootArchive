810c1f3543d68569d76d58958e5e80cad96d4c34
ckl <nanji@daikyn.com>
2026-07-28 13:06:09 +0800

    chore: v3.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.nanji.lootarchive"
         minSdk = 31
         targetSdk = 36
-        versionCode = 122
-        versionName = "3.2.22"
+        versionCode = 330
+        versionName = "3.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
